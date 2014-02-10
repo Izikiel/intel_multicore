f54e08e726dc4f6ba3456c4f220abcbe1d927884
Ezequiel Dario Gambaccini <izikielrules@gmail.com>
2014-02-10 18:46:48 -0300

    Mejore el codigo de test para ver de usar arrays multidimensionales estaticos

--- DeliriOS_64bits/test_code_data/generate_test_data.py
+++ DeliriOS_64bits/test_code_data/generate_test_data.py
@@ -1,28 +1,46 @@
 from random import shuffle
 
-def generate_random_array(size):
-	arr = range(0, size)
-	shuffle(arr)
 
-	return arr
+def generate_random_array(size):
+    arr = range(0, size)
+    shuffle(arr)
+
+    return arr
+
 
 def generate_test_data():
-	data = []
-	with open("test_data.c", "w") as t_data_file:
-		size = 2
+    data = []
+    with open("test_data.c", "w") as t_data_file:
+        size = 2
+        i = 0
+        while size < 4 * 1024 * 1024:
+            data.append(generate_string_array(i, size))
+            size *= 2
+            i += 1
 
-		while size < 4*1024*1024:
-			data.append(generate_random_array(size))
-			size *= 2
+        header = "#include <test_data.h>\n"
+        str_data = header + "".join(data) + \
+                   "".join(["test_data[%d] = test%d;\n"%(j,j) for j in xrange(len(data))])
+        t_data_file.write(str_data)
 
-		header = "#include <types.h> \n uint32_t test_data[%d][] = "%len(data)
-		str_data = str(data)
-		str_data = str_data.replace("[","\n{")
-		str_data = str_data.replace("]","}")
-		str_data += ";"
-		str_data = header + str_data
-		t_data_file.write(str_data)
+    with open("test_data.h","w") as header_file:
+        header_file.write("#include <types.h>\n\n")
+        header_file.write("uint32_t test_data[%d][];"%len(data))
+
+
+def generate_string_array(i, size):
+    arr = generate_random_array(size)
+    arr = str(arr)
+    arr = arr.replace("[", "{")
+    arr = arr.replace("]", "}")
+    arr += ";\n"
+
+    header = "uint32_t test%d[%d] = " % (i, size)
+
+    arr = header + arr
+
+    return arr
 
 
 if __name__ == '__main__':
-	generate_test_data()
+    generate_test_data()
---
+++ DeliriOS_64bits/test_code_data/test_data.h
@@ -0,0 +1,3 @@
+#include <types.h>
+
+uint32_t test_data[21][];
