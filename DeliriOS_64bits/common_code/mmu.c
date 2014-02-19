677bf9cb50d37109bac73e81b8de47819aff747e
Izikiel <izikielrules@gmail.com>
2014-02-19 11:32:18 -0300

    Bueno, ahora tiene stacks para 16 cores en total, se los tendría q bancar.

--- DeliriOS_64bits/common_code/mmu.c
+++ DeliriOS_64bits/common_code/mmu.c
@@ -4,14 +4,21 @@
 void* kernelStackPtrBSP = (void*)KERNEL_STACK_PTR_BSP;
 
 void* core_stack_ptrs[16] = {(void*) KERNEL_STACK_PTR_BSP,
-						(void*) KERNEL_STACK_PTR_AP1,
-						(void*) KERNEL_STACK_PTR_AP2,
-						(void*) KERNEL_STACK_PTR_AP3,
-						(void*) KERNEL_STACK_PTR_AP4,
-						(void*) KERNEL_STACK_PTR_AP5,
-						(void*) KERNEL_STACK_PTR_AP6,
-						(void*) KERNEL_STACK_PTR_AP7,
-						(void*) KERNEL_STACK_PTR_AP8,};
+							 (void*) KERNEL_STACK_PTR_AP1,
+							 (void*) KERNEL_STACK_PTR_AP2,
+							 (void*) KERNEL_STACK_PTR_AP3,
+							 (void*) KERNEL_STACK_PTR_AP4,
+							 (void*) KERNEL_STACK_PTR_AP5,
+							 (void*) KERNEL_STACK_PTR_AP6,
+							 (void*) KERNEL_STACK_PTR_AP7,
+							 (void*) KERNEL_STACK_PTR_AP8,
+							 (void*) KERNEL_STACK_PTR_AP9,
+							 (void*) KERNEL_STACK_PTR_AP10,
+							 (void*) KERNEL_STACK_PTR_AP11,
+							 (void*) KERNEL_STACK_PTR_AP12,
+							 (void*) KERNEL_STACK_PTR_AP13,
+							 (void*) KERNEL_STACK_PTR_AP14,
+							 (void*) KERNEL_STACK_PTR_AP15};
 
 /*
 	Estoy usando paginacion IA-32e => bits CR0.PG=1 + CR4.PAE=1 + EFER.LME=1
